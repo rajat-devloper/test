52703551e8bc32ecb106520f684371b3e459d5ee
rajat-devloper <rajat@donkeyapp.io>
2024-08-28 18:01:11 +0530

    minor commit

--- lasttest/last.js
+++ lasttest/last.js
@@ -1,36 +1,23 @@
-exports.handler = async (event) => {
-  try {
-    const inputText = event.body;
-    const parts = inputText.split(';');
+import json
 
-    if (parts.length < 5) {
-      throw new Error('Input text does not contain enough semicolons');
+def lambda_handler(event, context):
+    # input from request event
+    message = event['body']
+
+    # Extracting data between 3rd and 4th semicolons
+    data = message.split(';')[3]
+
+    # Extracting DOB and Passport number
+    pasbirth = data[31:39]  
+    pasdocnu = data[39:51].strip()
+
+    # Generate json output
+    output = {
+        "PassportNumber": pasdocnu,
+        "DateOfBirth": pasbirth
     }
-
-    const rawMessage = parts[3];
-
-    const pasbirth = rawMessage.slice(31, 39);
-    const pasdocnu = rawMessage.slice(39, 51).trim();
-
-    const outputJSON = {
-      PassportNumber: pasdocnu,
-      DateOfBirth: pasbirth
-    };
-
+    
     return {
-      statusCode: 200,
-      body: JSON.stringify(outputJSON),
-      headers: {
-        'Content-Type': 'application/json'
-      },
-    };
-  } catch (error) {
-    return {
-      statusCode: 500,
-      body: JSON.stringify({
-        message: 'Internal Server Error',
-        error: error.message,
-      }),
-    };
-  }
-};
+        'statusCode': 200,
+        'body': json.dumps(output)
+    }
